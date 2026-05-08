64db76e05e88ff900e5fca58423fcbd3166b742d
TOPFEROS-TECH <jackslinp@gmail.com>
2026-05-08 01:29:23 -0400

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ OWNER_NAME: "TOPFEROS TECH",
 
 PREFIX: ".",
 
-OWNER_NUMBER: "50900000000",
+OWNER_NUMBER: "50934640464",
 
 PHOTO_URL:
 "https://drive.google.com/uc?export=view&id=1-ONk_ZlyFGy3ne7rmZJkwk-8pcwB9WMJ"
