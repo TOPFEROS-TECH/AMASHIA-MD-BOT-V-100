3a7c911e0051e4d131b4e0da1a6ece72830d97a2
TOPFEROS-TECH <jackslinp@gmail.com>
2026-05-08 00:39:43 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+BOT_NAME: "AMASHIA MD V1.0.0",
+
+OWNER_NAME: "TOPFEROS TECH",
+
+PREFIX: ".",
+
+OWNER_NUMBER: "50900000000",
+
+PHOTO_URL:
+"https://drive.google.com/uc?export=view&id=1-ONk_ZlyFGy3ne7rmZJkwk-8pcwB9WMJ"
+
+}
